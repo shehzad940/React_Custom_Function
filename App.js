d0482e3e6d1c79fe78a6da6ea36fd2e2d5be5297
Mohd Shehzad <shehzad311997@gmail.com>
2019-02-06 17:27:41 +0530

    Update App.js

--- App.js
+++ App.js
@@ -9,15 +9,17 @@ class Parent extends React.Component {
     this.updateState = this.updateState.bind(this);
   }
 
-  updateState(key, val) {
-    this.setState(function (state) {
-      for (let i in state) {
-        if (i == key) {
-          state[i] = val;
+  updateState(newStateObj) {
+    // A custom function for updating state from child component like setState()
+    let newObjKeys = Object.keys(newStateObj);
+    this.setState(function(state) {
+      for (let key in state) {
+        if (newObjKeys.indexOf(key) != -1) {
+          state[key] = newStateObj[key];
         }
       }
       return state;
-    })
+    });
   }
 
   render() {
@@ -38,14 +40,20 @@ class Child extends React.Component {
 
     };
   }
+  
+  updateParentState() {
+    this.props.updateState({toggle: 'nope', text: 'some new text'});
+  }
+  
 
   render() {
     return (
       <div>
         <p>State toggle = {this.props.toggle}</p>
         <p>State text = {this.props.text}</p>
-        <button onClick={() => this.props.updateState("toggle", "nope")}>Update Toggle</button>
-        <button onClick={() => this.props.updateState("text", "some new text")}>Update Text</button>
+        <button onClick={() => this.props.updateState(toggle: "nope")}>Update Toggle</button>
+        <button onClick={() => this.props.updateState(text: "some new text")}>Update Text</button>
+        <button onClick={this.updateParentState.bind(this)}>Update both</button>
       </div>
     )
 
